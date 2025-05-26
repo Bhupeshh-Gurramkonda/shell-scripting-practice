acef1d00394a990dad24192de77b811eeb1c8d96
Bhupeshh <gurramkondabhupeshh@gmail.com>
2025-05-26 17:05:36 +0530

    third commit

---
+++ variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+var1=Boss
+var2=Emp
+
+echo "$var1:: Hey $var2, r u free?"
+echo "$var2:: $var1, I am working on task which u asked to complete ASAP."
+echo "$var1:: ok, do it fast i have another task to complete on emergency."
+echo "$var2:: ooreyyy.., pichi pulka."
