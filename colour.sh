84eb7b3fc8fe4f5c8e24e691080da200ca35a727
Bhupeshh <gurramkondabhupeshh@gmail.com>
2025-06-01 16:19:48 +0530

    twelveth commit

--- colour.sh
+++ colour.sh
@@ -3,16 +3,16 @@
 #checking the user is having the superuser permission or not.
 userid=$(id -u)
 
-R= echo -e "\e[31m"
-G= echo -e "\e[32m"
-Y= echo -e "\e[33m"
-W= echo -e "\e[0m"
+R= "\e[31m"
+G= "\e[32m"
+Y= "\e[33m"
+W= "\e[0m"
 
 if [ $userid -eq 0 ]
 then
-    echo " $G the user  having superuser permission to run script $W."
+    echo -e " $G the user  having superuser permission to run script $W."
 else
-    echo " $R the user doesn't have superuser permission to run script $W."
+    echo -e " $R the user doesn't have superuser permission to run script $W."
     exit 1
 fi 
 
@@ -22,9 +22,9 @@ packageinstalling(){
     dnf install $1 -y
     if [ $? -eq 0 ]
     then
-        echo " $G the package $1 has been installed successfully $W."
+        echo -e " $G the package $1 has been installed successfully $W."
     else
-        echo "$R the package installation has been failed. Please check the package name once $W."
+        echo -e "$R the package installation has been failed. Please check the package name once $W."
         exit 1
     fi
 }
@@ -32,10 +32,10 @@ packageinstalling(){
 packagevalidate(){
     if [ $? -eq 0 ]
     then
-        echo " $Y package $2 is already installed $W."
+        echo -e " ${$Y} package $2 is already installed ($W)."
         exit 1
     else
-        echo " $R package is not installed need to be install $W."
+        echo -e " $R package is not installed need to be install $W."
         packageinstalling $2
     fi
 }
